43c4ecda8bd02e28aa55ce8e4baf84029d939616
Mehmet Suzen <suzen@acm.org>
2014-11-18 18:06:10 +0000

    reddit

---
+++ src/github.com/dupree-cy/go_works/reddit/reddit.go
@@ -0,0 +1,63 @@
+package reddit
+
+import (
+    //"io"
+    "log"
+    "net/http"
+    //"os"
+    "errors"
+    "encoding/json"
+    "fmt"
+)
+
+
+type Stringer interface {
+  String() string
+}
+
+type Item struct {
+  Title    string
+  URL      string
+  Comments int `json:"num_comments"`
+}
+
+type Response struct {
+    Data struct {
+        Children []struct {
+            Data Item
+        }
+    }
+}
+
+func Get(reddit string) ([]Item, error) {
+    url       := fmt.Sprintf("http://reddit.com/r/%s.json", reddit)
+    resp, err := http.Get(url)                               
+    if err != nil {
+        return nil, err
+    }
+    defer resp.Body.Close()              
+    if resp.StatusCode != http.StatusOK {
+        return nil, errors.New(resp.Status)
+    }
+    r  := new(Response)                        
+    err = json.NewDecoder(resp.Body).Decode(r)
+    if err != nil {
+        return nil, err
+    }
+    items := make([]Item, len(r.Data.Children))
+    for i, child := range r.Data.Children {    
+      items[i] = child.Data
+    }
+    return items, nil
+}
+
+
+func main() {
+    items, err := Get("golang")
+    if err != nil {
+      log.Fatal(err)
+    }
+    for _, item := range items {
+        fmt.Println(item.Title)
+    }
+}
